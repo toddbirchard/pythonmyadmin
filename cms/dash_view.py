0a0fde346a19b9e549198d84f681f2e42f7ac83f
Todd Birchard <toddbirchard@gmail.com>
2019-05-09 21:47:51 -0400

    filter

--- cms/dash_view.py
+++ cms/dash_view.py
@@ -2,6 +2,7 @@ from dash import Dash
 import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import pandas as pd
 from flask import current_app as app
 from flask import Flask
@@ -10,13 +11,13 @@ from . import models
 
 def Add_Dash(server):
     """Plot.ly Dash view which populates the screen with loaded DataFrames."""
-
-
     external_stylesheets = ['/static/dist/css/plotly-flask-tutorial.css',
                             'https://fonts.googleapis.com/css?family=Lato',
                             'https://use.fontawesome.com/releases/v5.8.1/css/all.css']
+    external_scripts = ['/static/dist/js/includes/jquery.min.js', '/static/dist/js/main.js']
     dash_app = Dash(server=server,
                     external_stylesheets=external_stylesheets,
+                    external_scripts=external_scripts,
                     routes_pathname_prefix='/commands/')
 
     # Override the underlying HTML template
@@ -34,7 +35,15 @@ def Add_Dash(server):
                   <a href="/dash_view/"><i class="fas fa-chart-line"></i> MEME BOT MAFIA</a>
                 </nav>
                 <div style="width:95%; margin: 100px auto;">
-                {%app_entry%}
+                    <div class="filter">
+                        <span>Filter by type:</span>
+                        <button id="avi-filter">avi</button>
+                        <button id="basic-filter">basic</button>
+                        <button id="crypto-filter">crypto</button>
+                        <button id="goal-filter">goal</button>
+                        <button id="etc-filter">etc</button>
+                    </div>
+                    {%app_entry%}
                 </div>
                 <footer>
                     {%config%}
@@ -44,29 +53,40 @@ def Add_Dash(server):
             </body>
         </html>'''
 
-
+    cmd_df = get_dataset()
 
     # Create Dash Layout comprised of Data Tables
     dash_app.layout = html.Div(
-        children=get_dataset(),
+        children=[cmd_df, html.Div(id='datatable-filter-container')],
         id='flex-container'
       )
 
     return dash_app.server
 
 
-
-
 def get_dataset():
     """Return previews of all CSVs saved in /data directory."""
     data = models.Command.query.all()
     cmd_df = pd.DataFrame([(d.command, d.response, d.type) for d in data],
-                           columns=['command', 'response', 'type'])
+                          columns=['command', 'response', 'type'])
     # cmd_df.sort_values('type', axis=1, ascending=True)
     table_preview = dash_table.DataTable(
         id='commands',
         columns=[{"name": i, "id": i} for i in cmd_df.columns],
         data=cmd_df.to_dict("rows"),
         sorting=True,
+        # filtering=True,
     )
     return table_preview
+
+
+'''@dash_app.callback(
+    Output('datatable-filter-container', "children"),
+    [Input('datatable-filtering-fe', "data")])
+def update_graph(rows):
+    if rows is None:
+        dff = cmd_df
+    else:
+        dff = pd.DataFrame(rows)
+
+    return html.Div()'''
